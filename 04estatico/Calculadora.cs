1852f26ec3d3471b3f51e0cbb9f3e274bbf99171
davidtav <david.mclaurel@outlook.com.br>
2026-03-29 01:45:27 -0300

    criação e uso de  atributo e método estático em C#

---
+++ 04estatico/Calculadora.cs
@@ -0,0 +1,15 @@
+
+namespace _04estatico
+{
+    public class Calculadora
+    {
+		//Atributo numerico 
+        public static int numero = 10;
+		
+		//Metodo para somar dois valores
+		public static void Soma(int n1, int n2)
+		{
+			Console.WriteLine($"A soma dos valores e: {n1 + n2}");
+		}
+    }
+}
---
+++ 04estatico/Program.cs
@@ -0,0 +1,7 @@
+﻿using _04estatico;
+
+//exibir o valor contido no atributo numero
+Console.WriteLine($"O resultado da soma e:{Calculadora.numero}");
+
+//Realizar a soma
+Calculadora.Soma(8,5);
